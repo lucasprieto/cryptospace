dfed6e339e377226f032c0ecd1f98e096b9b186d
Lucas Prieto <lucasprieto12@gmail.com>
2021-06-30 05:36:30 -0300

    Basic permanent drawer

---
+++ src/components/DashLayout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Sidebar from './Sidebar';
+import ApplicationBar from './ApplicationBar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function DashLayout({ children }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <ApplicationBar />
+      <Sidebar />
+      <main className={classes.content}>
+        <Toolbar />
+        {children}
+      </main>
+    </div>
+  );
+}
+
+export default DashLayout
